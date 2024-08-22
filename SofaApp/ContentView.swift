e2897158a242f527f5b246047ea1d85a034b1870
iSAD <bradx2713@gmail.com>
2024-08-22 07:40:36 +0200

    Initial Commit

---
+++ SofaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SofaApp
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SofaApp/SofaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofaAppApp.swift
+//  SofaApp
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SofaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
